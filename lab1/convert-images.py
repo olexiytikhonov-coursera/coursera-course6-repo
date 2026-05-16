42c5c90d7dc8538b0c374c3fad583e5fc708d022
olexiytikhonov-coursera <olexiy.tikhonov@my.mertoncentre.ca>
2026-05-16 18:08:29 -0400

    setup venv and work with files

--- lab1/convert-images.py
+++ lab1/convert-images.py
@@ -2,3 +2,10 @@
 
 import os
 from PIL import Image
+
+files = os.listdir(os.getcwd()+'/images')
+for file in files:
+  if not file.startswith('.'):
+    abspath = os.path.abspath('images/' + file)
+    print(abspath)
+
