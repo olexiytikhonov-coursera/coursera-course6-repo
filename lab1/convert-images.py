ef5d85252973c5094df5585ef1d54a1e4c7e69bc
olexiytikhonov-coursera <olexiy.tikhonov@my.mertoncentre.ca>
2026-05-17 20:24:20 -0400

    add program description

--- lab1/convert-images.py
+++ lab1/convert-images.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+'lab1 resize and convert images t jpeg'
+
 import os
 from PIL import Image
 
@@ -7,5 +9,9 @@ files = os.listdir(os.getcwd()+'/images')
 for file in files:
   if not file.startswith('.'):
     abspath = os.path.abspath('images/' + file)
-    print(abspath)
-
+    img = Image.open(abspath)
+    outfile = "/opt/icons/" + os.path.splitext(file)[0] + ".jpeg"
+#    outfile = os.path.abspath("./images_new/"+os.path.splitext(file)[0] + ".jpeg")
+    out = img.rotate(-90).resize((128,128)).convert("RGB")
+    out.save(outfile, "JPEG")
+    img.close()
